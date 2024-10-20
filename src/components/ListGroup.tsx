a50f5ac7a708a328e99d9b6a3df0fa161df791f8
Ananya Sikdar <iamananyasikdar@gmail.com>
2024-10-20 20:47:01 +0530

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -10,7 +10,7 @@ function ListGroup()
         'Paris'
     ];
 
-    items = [];
+    
 
     /* Conditional Rendering
     if(items.length === 0)
@@ -30,7 +30,7 @@ function ListGroup()
             { items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
             {items.map((item) => (
-                <li key={item}>{item}</li>
+                <li className="list-group-item" key={item} onClick={(event) => console.log(event)}>{item}</li>
                 ))} 
               </ul>
         </>
