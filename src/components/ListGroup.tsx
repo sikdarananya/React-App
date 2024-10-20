27d224db054985c6913d1b43ff83df4d336c7dde
Ananya Sikdar <iamananyasikdar@gmail.com>
2024-10-20 20:37:15 +0530

    ConditionalRendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,7 +2,7 @@
 
 function ListGroup()
 {
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
@@ -10,10 +10,24 @@ function ListGroup()
         'Paris'
     ];
 
+    items = [];
+
+    /* Conditional Rendering
+    if(items.length === 0)
+        return  
+    (
+        <>
+        <h1>List</h1>
+        <p>No item found</p>
+        </>
+    );
+    */
+
     
     return (
         <>
             <h1>List</h1>
+            { items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
             {items.map((item) => (
                 <li key={item}>{item}</li>
